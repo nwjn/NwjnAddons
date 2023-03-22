83687f82fc5304870c44fce41d5d8d5f60414be8
nwjn <127889040+nwjn@users.noreply.github.com>
2023-03-21 23:44:32 -0700

    umm actually its this one

--- features/bestiary.js
+++ features/bestiary.js
@@ -1,19 +1,10 @@
 import Settings from "../config";
 // import "./utils/entities"
 
-
-// Bestiary Tracker, Tracks the bestiary mobs of the island you're in, needs to grab all possible regions of island from sidebar location
-
-// Hub: Wilderness: Spooky | Ruins, Graveyard: Wolves, Zombies
-// Park: Howling Cave: Wolves
-// Den: Spider's Den: Spiders, slimes, and skeletons
-// End: The End, Bruiser's Hideout, Void Sepulture: Enderman, Watcher, Obsidian Defender, Zealot, Endermite, Dragon, Golem, Voidling Fanatic, Voidling Extremist
-// Crimson Isle
-
 let inSB = false;
 
 // make it so it ticks a little after worldload somehow or 
-// "just use a tick trigger or something to check the scoreboard and once it's found then set inSkyblock to true or whatever and stop checking until the next worldLoad"
+// "just use a tick trigger or something to check the scoreboard and once it's found then set inSkyblock to true or whatever and stop checking until the next worldLoad" - dude from Chattriggers discord
 register("tick", () => {
   let Title = Scoreboard.getTitle().removeFormatting();
   // console.log(Title)
@@ -28,6 +19,4 @@ register("tick", () => {
     inSB = false;
     console.log("Should be false:", inSB)
   }
-  register("worldUnload", () => {
-  })
 })
