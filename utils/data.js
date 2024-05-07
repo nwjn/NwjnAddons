34ba2c02d32b33b50edda0e0b36547baa5c6629b
nwjn <127889040+nwjn@users.noreply.github.com>
2024-05-06 22:03:41 -0700

    0.11 pre

--- utils/data.js
+++ utils/data.js
@@ -2,7 +2,7 @@ import PogObject from "PogData"
 
 export let data = new PogObject("NwjnAddons", {
   "first_time": true,
-  "version": "0.10.2",
+  "version": "0.11.0",
   "power": "Unknown",
   "tuning": "Unknown",
   "enrich": "Unknown",
