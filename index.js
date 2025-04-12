60f1dbbccb5dea78db0bd22b24b31dfb2f02fbc2
Cameron Lee <127889040+nwjn@users.noreply.github.com>
2025-04-12 14:52:43 -0700

    Command: Fix opening gui and change formatting

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+global.NWJN = "§r§0§l 【§r §c§lNwjn§0§l 】§r"
 import "./data/Data"
 import "./utils/Broadcasting"
 import "./utils/Command"
@@ -7,7 +8,7 @@ import "./utils/Profile"
 import "./libs/Time/ServerTime"
 
 // Handles loading all Feature files because I was too lazy to type them all out
-let pathFinder = /NwjnAddons[\/\\]features[\/\\](.+[\/\\]\w+)\.js$/
+let pathFinder = /Nwjn[\/\\]features[\/\\](.+[\/\\]\w+)\.js$/
 let fileSeparator = /\\/g
 let relativeDest = "./features/"
 
@@ -19,7 +20,7 @@ void function requireFeatures(file) {
     if (!match) return
     
     modules.push(relativeDest + match[1].replace(fileSeparator, "/"))
-}(new java.io.File(`${Config.modulesFolder}/NwjnAddons/features`))
+}(new java.io.File(`${Config.modulesFolder}/Nwjn/features`))
 
 let module
 while (module = modules.pop()) {
--- utils/Command.js
+++ utils/Command.js
@@ -2,11 +2,14 @@ import Settings from "../data/Settings"
 import { CommandHandler } from "../../tska/command/CommandHandler"
 
 const myCommands = new CommandHandler("Nwjn")
+
     // Default setup to open module gui
-    .setName("nwjn", () => Settings.getConfig().openGui() && 1)
+    .setName("Nwjn", (arg) => !arg && Settings.getConfig().openGui() && 1)
+
     // Add custom formatting
-    .setTitleFormat("§0§l[§c§l${name}§0§l]§r §6Commands:")
-    .setCommandFormat("   [§n§b/${name}§r]§f: §7${description}")
+    .setTitleFormat(ChatLib.getCenteredText(`&0&m&l---------${NWJN}&0&m&l---------`))
+    .setCommandFormat("  • §n§b/${name}§r§f: §7${description}")
+
 
 export function addCommand(command, description, cb) {
     myCommands.push(command, description, cb)
