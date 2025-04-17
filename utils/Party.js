f91ee64607d851d4194a3e71a4d5a7d24f27c83a
Cameron Lee <127889040+nwjn@users.noreply.github.com>
2025-04-16 19:00:52 -0700

    Party: Assign regexes

--- utils/Party.js
+++ utils/Party.js
@@ -3,6 +3,18 @@ import ModAPI from "../../tska/api/ModAPI"
 import { scheduleTask } from "../libs/Time/ServerTime"
 import Seconds from "../libs/Time/Units/Seconds"
 
+const SERVER_IP_REGEX = /^[^\.]+\.hypixel\.net$/
+const START_PARTY_REGEX = /^(?:\[\w+\+*\] )?\w{1,16} invited .+ to the party! They have 60 seconds to accept\.$/
+const TRANSFER_PARTY_REGEX = /^The party was transferred to (?:\[\w+\+*\] )?(\w{1,16}) because .+ left$/
+const DISBAND_PARTY_REGEXES = [
+    "(?:\\[\\w+\\+*\\] )?\\w{1,16} has disbanded the party!",
+    "The party was disbanded because all invites expired and the party was empty\\.",
+    "You left the party\\.",
+    "You are not currently in a party\\.",
+    "You have been kicked from the party by .+",
+    "The party was disbanded because the party leader disconnected\\."
+]
+
 export default new class Party {
 	constructor() {
         /** @private */ this._isLead = false
@@ -15,16 +27,16 @@ export default new class Party {
         
         new Event("serverConnect", this._request.bind(this))
 
-        new Event("serverChat", () => this._inParty = this._isLead = false, /^(?:\[\w+\+*\] )?\w{1,16} has disbanded the party!|The party was disbanded because all invites expired and the party was empty.|You left the party\.|You are not currently in a party\.|You have been kicked from the party by .+|The party was disbanded because the party leader disconnected\.$/)
-        new Event("serverChat", () => this._isLead = this._isLead || !this._inParty, /^(?:\[\w+\+*\] )?\w{1,16} invited .+ to the party! They have 60 seconds to accept\.$/)
-        new Event("serverChat", (leader) => this._isLead = Player.getName() === leader.removeFormatting(), /^The party was transferred to (?:\[\w+\+*\] )?(\w{1,16}) because .+ left$/)
+        new Event("serverChat", () => this._inParty = this._isLead = false, new RegExp(`^${DISBAND_PARTY_REGEXES.join("|")}$`))
+        new Event("serverChat", () => this._isLead = this._isLead || !this._inParty, START_PARTY_REGEX)
+        new Event("serverChat", (leader) => this._isLead = Player.getName() === leader.removeFormatting(), TRANSFER_PARTY_REGEX)
         
         if (World.isLoaded()) this._request()
 	}
 
     /** @private */
     _request() {
-        if (!/^[^\.]+\.hypixel\.net$/.test(Server.getIP())) return
+        if (!SERVER_IP_REGEX.test(Server.getIP())) return
 
         scheduleTask(ModAPI.requestPartyInfo.bind(ModAPI), Seconds.of(2))
     }
