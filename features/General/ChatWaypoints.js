98133403c3131aabd9a0b6b29d4b49ad3f28d729
Cameron Lee <127889040+nwjn@users.noreply.github.com>
2025-04-22 09:37:26 -0700

    Remove gitignored files

--- features/General/ChatWaypoints.js
+++ features/General/ChatWaypoints.js
@@ -1,73 +1,73 @@
-import Feature from "../../libs/Features/Feature"
-import TextUtil from "../../libs/Helper/TextUtil"
-import Settings from "../../data/Settings"
-import Waypoint from "../../libs/Render/TempWaypoint"
-import Ticks from "../../libs/Time/Units/Ticks"
-import Data from "../../data/Data"
-import Nwjn from "../../libs/Helper/Nwjn"
+// import Feature from "../../libs/Features/Feature"
+// import TextUtil from "../../libs/Helper/TextUtil"
+// import Settings from "../../data/Settings"
+// import Waypoint from "../../libs/Render/TempWaypoint"
+// import Ticks from "../../libs/Time/Units/Ticks"
+// import Data from "../../data/Data"
+// import Nwjn from "../../libs/Helper/Nwjn"
 
-new class ChatWaypoints extends Feature {
-    constructor() {
-        super({
-            setting: this.constructor.name, 
-            color: this.constructor.name + "Color"
-        })
+// new class ChatWaypoints extends Feature {
+//     constructor() {
+//         super({
+//             setting: this.constructor.name, 
+//             color: this.constructor.name + "Color"
+//         })
 
-        this.waypoints = new Map()
+//         this.waypoints = new Map()
 
-        this.addEvent(
-            "serverChat", 
-            this.onWaypointSent.bind(this), 
-            /^(?:[\w\-]{5} > )?(?:\[\d{1,3}\] .? ?)?(?:\[\w+\+*\] )?(\w{1,16})(?: .? ?)?: x: (-?[\d\.]+), y: (-?[\d\.]+), z: (-?[\d\.]+) ?(.+)?$/
-        )
+//         this.addEvent(
+//             "serverChat", 
+//             this.onWaypointSent.bind(this), 
+//             /^(?:[\w\-]{5} > )?(?:\[\d{1,3}\] .? ?)?(?:\[\w+\+*\] )?(\w{1,16})(?: .? ?)?: x: (-?[\d\.]+), y: (-?[\d\.]+), z: (-?[\d\.]+) ?(.+)?$/
+//         )
         
-        this.addSubEvent(
-            "interval", 
-            this.onIntervalPassed.bind(this),
-            Ticks.of(3), 
-            () => this.waypoints.size
-        )
+//         this.addSubEvent(
+//             "interval", 
+//             this.onIntervalPassed.bind(this),
+//             Ticks.of(3), 
+//             () => this.waypoints.size
+//         )
         
-        this.addSubEvent(
-            "renderWorld", 
-            this.onRenderWorld.bind(this),
-            null, 
-            () => this.waypoints.size
-        )
+//         this.addSubEvent(
+//             "renderWorld", 
+//             this.onRenderWorld.bind(this),
+//             null, 
+//             () => this.waypoints.size
+//         )
 
-        this.init()
-    }
+//         this.init()
+//     }
 
-    onWaypointSent(displayName, x, y, z, text = "", event, formatted) {
-        const ign = TextUtil.getSenderName(displayName).toLowerCase()
+//     onWaypointSent(displayName, x, y, z, text = "", event, formatted) {
+//         const ign = TextUtil.getSenderName(displayName).toLowerCase()
 
-        if (ign in Data.blacklist) return Nwjn.append(event./* getChatComponent */func_148915_c(), "§cBlacklisted")
+//         if (ign in Data.blacklist) return Nwjn.append(event./* getChatComponent */func_148915_c(), "§cBlacklisted")
         
-        const [mainText] = TextUtil.getMatches(/^(.+)§.:/, formatted)
-        if (!mainText) return
+//         const [mainText] = TextUtil.getMatches(/^(.+)§.:/, formatted)
+//         if (!mainText) return
 
-        this.waypoints.set(ign, new Waypoint(mainText, text, x, y, z, 5, Settings.ChatWaypointsTime))
-        this.updateSubEvents()
-    }
+//         this.waypoints.set(ign, new Waypoint(mainText, text, x, y, z, 5, Settings.ChatWaypointsTime))
+//         this.updateSubEvents()
+//     }
 
-    onIntervalPassed() {
-        this.waypoints.forEach((waypoint, id) => {
-            waypoint.update()
+//     onIntervalPassed() {
+//         this.waypoints.forEach((waypoint, id) => {
+//             waypoint.update()
 
-            if (!waypoint.dirty) return
+//             if (!waypoint.dirty) return
             
-            this.waypoints.delete(id)
-            this.updateSubEvents()
-        })
-    }
+//             this.waypoints.delete(id)
+//             this.updateSubEvents()
+//         })
+//     }
 
-    onRenderWorld() {
-        this.waypoints.forEach(waypoint => 
-            waypoint.render(this.Color)
-        )
-    }
+//     onRenderWorld() {
+//         this.waypoints.forEach(waypoint => 
+//             waypoint.render(this.Color)
+//         )
+//     }
 
-    onDisabled() {
-        this.waypoints.clear()
-    }
-}
+//     onDisabled() {
+//         this.waypoints.clear()
+//     }
+// }
--- features/General/PartyCommands.js
+++ features/General/PartyCommands.js
@@ -1,183 +1,183 @@
-import Feature from "../../libs/Features/Feature"
-import Party from "../../utils/Party"
-import Data from "../../data/Data"
-import TextUtil from "../../libs/Helper/TextUtil"
-import Location from "../../utils/Location"
-import Settings from "../../data/Settings"
-import { getTPS, scheduleTask } from "../../libs/Time/ServerTime"
-import Nwjn from "../../libs/Helper/Nwjn"
+// import Feature from "../../libs/Features/Feature"
+// import Party from "../../utils/Party"
+// import Data from "../../data/Data"
+// import TextUtil from "../../libs/Helper/TextUtil"
+// import Location from "../../utils/Location"
+// import Settings from "../../data/Settings"
+// import { getTPS, scheduleTask } from "../../libs/Time/ServerTime"
+// import Nwjn from "../../libs/Helper/Nwjn"
 
-class PartyCommand {
-    constructor(pattern, access, runnable) {
-        this.pattern = pattern
-        this.access = access
-        this.runnable = runnable
-    }
+// class PartyCommand {
+//     constructor(pattern, access, runnable) {
+//         this.pattern = pattern
+//         this.access = access
+//         this.runnable = runnable
+//     }
 
-    isAllowed(cmd) {
-        if (!this.pattern.test(cmd)) return false
-        if (!this.access()) return false
+//     isAllowed(cmd) {
+//         if (!this.pattern.test(cmd)) return false
+//         if (!this.access()) return false
 
-        return true
-    }
+//         return true
+//     }
 
-    run(sender, args) {
-        this.runnable(sender, args)
-    }
-}
+//     run(sender, args) {
+//         this.runnable(sender, args)
+//     }
+// }
 
-new class PartyCommands extends Feature {
-    constructor() {
-        super({setting: this.constructor.name})
+// new class PartyCommands extends Feature {
+//     constructor() {
+//         super({setting: this.constructor.name})
 
-        this.commandList = []
-        this.helpMessage = []
+//         this.commandList = []
+//         this.helpMessage = []
 
-        this.initCommands()
+//         this.initCommands()
 
-        this.addEvent(
-            "serverChat", 
-            this.onPartyCommandSent.bind(this), 
-            /^Party > ([^:]+): [_,.?!]([^\s]+)(.+)?$/
-        )
+//         this.addEvent(
+//             "serverChat", 
+//             this.onPartyCommandSent.bind(this), 
+//             /^Party > ([^:]+): [_,.?!]([^\s]+)(.+)?$/
+//         )
 
-        this.init()
-    }
+//         this.init()
+//     }
 
-    onPartyCommandSent(player, command, arg, event) {
-        const sender = TextUtil.getSenderName(player).toLowerCase()
-        if (sender in Data.blacklist) return Nwjn.append(event./* getChatComponent */func_148915_c(), "§cBlacklisted")
+//     onPartyCommandSent(player, command, arg, event) {
+//         const sender = TextUtil.getSenderName(player).toLowerCase()
+//         if (sender in Data.blacklist) return Nwjn.append(event./* getChatComponent */func_148915_c(), "§cBlacklisted")
 
-        const cmd = command.toLowerCase()
-        if (cmd === "help") return this.helpMessage()
+//         const cmd = command.toLowerCase()
+//         if (cmd === "help") return this.helpMessage()
 
-        for (let i = 0; i < this.commandList.length; i++) {
-            let pc = this.commandList[i]
+//         for (let i = 0; i < this.commandList.length; i++) {
+//             let pc = this.commandList[i]
             
-            if (!pc.isAllowed(cmd)) continue
+//             if (!pc.isAllowed(cmd)) continue
 
-            scheduleTask(() => pc.run(sender, arg, cmd))
-            break
-        }
-    }
+//             scheduleTask(() => pc.run(sender, arg, cmd))
+//             break
+//         }
+//     }
 
-    addCommand({usage, pattern, access, runnable}) {
-        this.commandList.push(new PartyCommand(pattern, access, runnable))
-        this.helpMessage.push(usage)
-    }
+//     addCommand({usage, pattern, access, runnable}) {
+//         this.commandList.push(new PartyCommand(pattern, access, runnable))
+//         this.helpMessage.push(usage)
+//     }
 
-    initCommands() {
-        this.addCommand({
-            usage: ".time",
-            pattern: /^time$/,
-            access: () => Settings.PartyCommandsTime,
-            runnable() {
-                const formatter = new java.text.SimpleDateFormat("E hh:mm:ss a z", java.util.Locale.US)
-                const response = formatter.format(Date.now())
+//     initCommands() {
+//         this.addCommand({
+//             usage: ".time",
+//             pattern: /^time$/,
+//             access: () => Settings.PartyCommandsTime,
+//             runnable() {
+//                 const formatter = new java.text.SimpleDateFormat("E hh:mm:ss a z", java.util.Locale.US)
+//                 const response = formatter.format(Date.now())
 
-                ChatLib.say(`/pc ${response}`)
-            }
-        })
+//                 ChatLib.say(`/pc ${response}`)
+//             }
+//         })
 
-        this.addCommand({
-            usage: ".coords",
-            pattern: /^coords?|loc|xyz$/,
-            access: () => Settings.PartyCommandsCoords,
-            runnable() {
-                const response = `x: ${~~Player.getX()}, y: ${~~Player.getY()}, z: ${~~Player.getZ()} [${Location.world} - ${Location.zone}]`
+//         this.addCommand({
+//             usage: ".coords",
+//             pattern: /^coords?|loc|xyz$/,
+//             access: () => Settings.PartyCommandsCoords,
+//             runnable() {
+//                 const response = `x: ${~~Player.getX()}, y: ${~~Player.getY()}, z: ${~~Player.getZ()} [${Location.world} - ${Location.zone}]`
 
-                ChatLib.say(`/pc ${response}`)
-            }
-        })
+//                 ChatLib.say(`/pc ${response}`)
+//             }
+//         })
 
-        this.addCommand({
-            usage: ".power",
-            pattern: /^pow(er)?$/,
-            access: () => Settings.PartyCommandsPower,
-            runnable() {
-                const response = `Power: ${ Data.power } | Tuning: ${ Data.tuning } | Enrich: ${ Data.enrich } | MP: ${ Data.mp }`
+//         this.addCommand({
+//             usage: ".power",
+//             pattern: /^pow(er)?$/,
+//             access: () => Settings.PartyCommandsPower,
+//             runnable() {
+//                 const response = `Power: ${ Data.power } | Tuning: ${ Data.tuning } | Enrich: ${ Data.enrich } | MP: ${ Data.mp }`
 
-                ChatLib.say(`/pc ${response}`)
-            }
-        })
+//                 ChatLib.say(`/pc ${response}`)
+//             }
+//         })
 
-        this.addCommand({
-            usage: ".stats",
-            pattern: /^stats?$/,
-            access: () => Settings.PartyCommandsStats,
-            runnable() {
-                const hypixelMoment = Location.inWorld("Catacombs") ? /Skills:/ : /Stats:/
-                const widget = TextUtil.getTabBlock(TabList.getNames(), hypixelMoment)
-                if (!widget) return Nwjn.chat("Cannot find stats widget in tab")
+//         this.addCommand({
+//             usage: ".stats",
+//             pattern: /^stats?$/,
+//             access: () => Settings.PartyCommandsStats,
+//             runnable() {
+//                 const hypixelMoment = Location.inWorld("Catacombs") ? /Skills:/ : /Stats:/
+//                 const widget = TextUtil.getTabBlock(TabList.getNames(), hypixelMoment)
+//                 if (!widget) return Nwjn.chat("Cannot find stats widget in tab")
 
-                const stats = widget.map(it => it.removeFormatting().match(/: (.[\d]+)$/)?.[1])
-                const response = stats.join(" | ")
+//                 const stats = widget.map(it => it.removeFormatting().match(/: (.[\d]+)$/)?.[1])
+//                 const response = stats.join(" | ")
 
-                ChatLib.say(`/pc ${response}`)
-            }
-        })
+//                 ChatLib.say(`/pc ${response}`)
+//             }
+//         })
 
-        this.addCommand({
-            usage: ".tps",
-            pattern: /^tps$/,
-            access: () => Settings.PartyCommandsTPS,
-            runnable() {
-                const tps = getTPS()
-                const response = `TPS: ${tps}`
+//         this.addCommand({
+//             usage: ".tps",
+//             pattern: /^tps$/,
+//             access: () => Settings.PartyCommandsTPS,
+//             runnable() {
+//                 const tps = getTPS()
+//                 const response = `TPS: ${tps}`
 
-                ChatLib.say(`/pc ${response}`)
-            }
-        })
+//                 ChatLib.say(`/pc ${response}`)
+//             }
+//         })
 
-        this.addCommand({
-            usage: ".allinv",
-            pattern: /^allinv(ite)?$/,
-            access: () => Settings.PartyCommandsAllInvite && Party.isLeader(),
-            runnable: () => ChatLib.say("/p settings allinvite")
-        })
+//         this.addCommand({
+//             usage: ".allinv",
+//             pattern: /^allinv(ite)?$/,
+//             access: () => Settings.PartyCommandsAllInvite && Party.isLeader(),
+//             runnable: () => ChatLib.say("/p settings allinvite")
+//         })
 
-        this.addCommand({
-            usage: ".inv <ign>",
-            pattern: /^inv(ite)?$/,
-            access: () => Settings.PartyCommandsInvite && Party.isLeader(),
-            runnable: (_, invitee) => ChatLib.say(`/p ${invitee}`)
-        })
+//         this.addCommand({
+//             usage: ".inv <ign>",
+//             pattern: /^inv(ite)?$/,
+//             access: () => Settings.PartyCommandsInvite && Party.isLeader(),
+//             runnable: (_, invitee) => ChatLib.say(`/p ${invitee}`)
+//         })
 
-        this.addCommand({
-            usage: ".warp",
-            pattern: /^warp$/,
-            access: () => Settings.PartyCommandsWarp && Party.isLeader(),
-            runnable: () => ChatLib.say("/p warp")
-        })
+//         this.addCommand({
+//             usage: ".warp",
+//             pattern: /^warp$/,
+//             access: () => Settings.PartyCommandsWarp && Party.isLeader(),
+//             runnable: () => ChatLib.say("/p warp")
+//         })
 
-        this.addCommand({
-            usage: ".pt <ign?>",
-            pattern: /^transfer|pt(me)?$/,
-            access: () => Settings.PartyCommandsTransfer && Party.isLeader(),
-            runnable(sender, ign) {
-                const target = ign ?? sender
-                ChatLib.say(`/p transfer ${target}`)
-            }
-        })
+//         this.addCommand({
+//             usage: ".pt <ign?>",
+//             pattern: /^transfer|pt(me)?$/,
+//             access: () => Settings.PartyCommandsTransfer && Party.isLeader(),
+//             runnable(sender, ign) {
+//                 const target = ign ?? sender
+//                 ChatLib.say(`/p transfer ${target}`)
+//             }
+//         })
         
-        this.addCommand({
-            usage: ".f1-7 | .m1-7 | .t1-5",
-            pattern: /^(f|m) ?[1-7]|t ?[1-5]$/,
-            access: () => Settings.PartyCommandsInstance && Party.isLeader(),
-            runnable(_, __, cmd) {
-                const [type, number] = TextUtil.getMatches(/^(f|m) ?[1-7]|t ?[1-5]$/, cmd)
+//         this.addCommand({
+//             usage: ".f1-7 | .m1-7 | .t1-5",
+//             pattern: /^(f|m) ?[1-7]|t ?[1-5]$/,
+//             access: () => Settings.PartyCommandsInstance && Party.isLeader(),
+//             runnable(_, __, cmd) {
+//                 const [type, number] = TextUtil.getMatches(/^(f|m) ?[1-7]|t ?[1-5]$/, cmd)
 
-                switch (type) {
-                    case 'f': return ChatLib.say(`/joininstance catacombs_floor_${TextUtil.getFloorWord(number)}`)
-                    case 'm': return ChatLib.say(`/joininstance master_catacombs_floor_${TextUtil.getFloorWord(number)}`)
-                    case 't': return ChatLib.say(`/joininstance kuudra_${TextUtil.getTierWord(number)}`)
-                    default:  return ChatLib.say("/pc Invalid instance?")
-                }
-            }
-        })
+//                 switch (type) {
+//                     case 'f': return ChatLib.say(`/joininstance catacombs_floor_${TextUtil.getFloorWord(number)}`)
+//                     case 'm': return ChatLib.say(`/joininstance master_catacombs_floor_${TextUtil.getFloorWord(number)}`)
+//                     case 't': return ChatLib.say(`/joininstance kuudra_${TextUtil.getTierWord(number)}`)
+//                     default:  return ChatLib.say("/pc Invalid instance?")
+//                 }
+//             }
+//         })
 
-        this.helpMessage = () => Nwjn.say(this.helpMessage.join(" | "))
-        delete this.addCommand
-        delete this.initCommands
-    }
-}
+//         this.helpMessage = () => Nwjn.say(this.helpMessage.join(" | "))
+//         delete this.addCommand
+//         delete this.initCommands
+//     }
+// }
