e3963b316a9aa75482017c4c6fc881eb91330bc3
Cameron Lee <127889040+nwjn@users.noreply.github.com>
2025-04-27 22:57:22 -0700

    Tired

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 // import "./libs/Helper/Command"
 // import "./utils/Location"
 // import "./utils/Party"
-import "./utils/Profile"
-import "./libs/Time/ServerTime"
+// import "./utils/Profile"
+// import "./libs/Time/ServerTime"
 
 // Handles loading all Feature files because I was too lazy to type them all out
 import Feature from "./libs/Features/Feature"
