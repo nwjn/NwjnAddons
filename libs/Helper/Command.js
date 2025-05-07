46d9182e814ed353f4ffd90d4ae143117dd117e6
Cameron Lee <127889040+nwjn@users.noreply.github.com>
2025-05-06 22:26:11 -0700

    Personal preference

--- libs/Helper/Command.js
+++ libs/Helper/Command.js
@@ -5,12 +5,12 @@ import { CommandHandler } from "../../../tska/command/CommandHandler"
 const myCommands = new CommandHandler("Nwjn")
 
     // Default setup to open module gui
-    .setName("Nwjn", (arg) => !arg && ConfigProperty.getConfig()?.openGui() && 1)
+    .setName("Nwjn", (arg) => !arg && !!ConfigProperty.getConfig()?.openGui())
 
     // Add custom formatting
     .setCommandFormat("  • §n§b/${name}§r§f: §7${description}")
-    .setErrorFormat(Nwjn.LOGO + "§cCommand §b§l/${arg}§r§c is unknown. Run §a§l/nwjn help§r§c.")
-    Object.defineProperty(myCommands, "titleFormat", {get: () => ChatLib.getCenteredText(Nwjn.BANNER)})
+    .setErrorFormat(`${Nwjn.LOGO}§cCommand §b§l/\$\{arg\}§r§c is unknown. Run §a§l/nwjn help§r§c.`)
+    Object.defineProperty(myCommands, "titleFormat", { get: () => ChatLib.getCenteredText(Nwjn.BANNER) })
 
 export function addCommand(command, description, cb) {
     myCommands.push(command, description, cb)
