12123968a2ecbbe0cf30e7393b6a2f9ebddf5813
Cameron Lee <127889040+nwjn@users.noreply.github.com>
2025-05-15 22:53:50 -0700

    Fix title format not correctly initializing as a getter

--- libs/Helper/Command.js
+++ libs/Helper/Command.js
@@ -2,31 +2,6 @@ import Nwjn from "./Nwjn"
 import ConfigProperty from "../../data/ConfigProperty"
 import { CommandHandler } from "../../../tska/command/CommandHandler"
 
-// const myCommands = new CommandHandler("Nwjn")
-
-//     // Default setup to open module gui
-//     .setName("Nwjn", (arg) => !arg && !!ConfigProperty.getConfig()?.openGui())
-
-//     // Add custom formatting
-//     .setCommandFormat("  • §n§b${name}§r§f: §7${description}")
-//     .setErrorFormat(`${Nwjn.LOGO}§cCommand §b§l/\$\{arg\}§r§c is unknown. Run §a§l/nwjn help§r§c.`)
-//     Object.defineProperty(myCommands, "titleFormat", { get: () => ChatLib.getCenteredText(Nwjn.BANNER) })
-
-// export function addCommand({ name, aliases, description, run, tabCompletions = null, clickAction = "suggest" }) {
-//     if (clickAction === "suggest") {
-//         let cb = run
-
-//         run = (...args) => {
-//             if (!args?.[0]) return Client.scheduleTask(() => Client.setCurrentChatMessage(`/nwjn ${name} `))
-//             cb(...args)
-//         }
-//     }
-
-//     myCommands.pushWithAlias(name, aliases, description, run)
-
-//     if (tabCompletions) myCommands.setTabCompletion(name, tabCompletions)
-// }
-
 export default new class extends CommandHandler {
     constructor() {
         super("Nwjn")
@@ -37,12 +12,9 @@ export default new class extends CommandHandler {
         // Add custom formatting
         this.setCommandFormat("  • §n§b${name}§r§f: §7${description}")
         this.setErrorFormat(`${Nwjn.LOGO}§cCommand §b§l/\$\{arg\}§r§c is unknown. Run §a§l/nwjn help§r§c.`)
+        Object.defineProperty(this, "titleFormat", { get: () => ChatLib.getCenteredText(Nwjn.BANNER) })
 
         this.ACTION = { SUGGEST: 0, RUN: 1 }
-    }
-
-    get titleFormat() {
-        return ChatLib.getCenteredText(Nwjn.BANNER)
     }
 
     addCommand({ name, aliases, description, run, tabCompletions = null, clickAction = this.ACTION.SUGGEST }) {
@@ -59,4 +31,4 @@ export default new class extends CommandHandler {
 
         if (tabCompletions) this.setTabCompletion(name, tabCompletions)
     }
-    }
+}
