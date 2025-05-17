73aeb929d8b6ee7f605c5399add80f2f0d71f898
Cameron Lee <127889040+nwjn@users.noreply.github.com>
2025-05-16 21:54:07 -0700

    Use outliner and disregard invis

--- features/Bestiary/MobHighlight.js
+++ features/Bestiary/MobHighlight.js
@@ -4,7 +4,8 @@ import MobUtil from "../../libs/Helper/MobUtil"
 import Feature from "../../libs/Features/Feature"
 import ConfigProperty from "../../data/ConfigProperty"
 import { getEntity } from "../../libs/Helper/ClassReference"
-import { createPerEntityOutliner, createCustomOutlineTester, createManualOutliner } from "../../../Apelles"
+import { createPerEntityOutliner, createCustomOutlineTester } from "../../../Apelles"
+import { scheduleTask } from "../../libs/Time/Scheduler"
 
 void new class extends Feature {    
     constructor() {        
@@ -28,29 +29,31 @@ void new class extends Feature {
             }),
 
             whiteList: new HashMap(),
-
-            outliner: createManualOutliner(0xffbeefff, 2)
+            tester: null,
+            outliner: null
         })
 
-        this.addEvent("EntityJoin", this.test.bind(this))
+        this.addEvent("PacketReceived", this.onStatus.bind(this), { setFilteredClass: "EntityEffect" })
+    }
+
+    onEffect(packet) {
+        if (packet.func_149427_e() === 14) scheduleTask(() => this.outliner.clear())
     }
 
     test(entity) {
+        if (entity./* isInvisible */func_82150_aj()) return
+
         entity = entity?.entity ?? entity
         const entClass = entity.class
         const isWhitelisted = this.whiteList.get(entClass)
-        if (!isWhitelisted) return false
+        if (!isWhitelisted) return
 
         const healthList = this.whiteList.get(entClass)
-        if (typeof(healthList) === "boolean" || healthList?.includes(MobUtil.getMaxHP(entity))) this.outliner.add(entity)
+        return healthList?.includes(MobUtil.getMaxHP(entity)) || healthList
     }
 
     onEnabled(value = this.setting.value) {
-        // this.outliner.clear()
-        World.getWorld()?./* loadedEntityList */field_72996_f?.forEach(e => this.outliner.remove(e))
-        this.whiteList.clear()
-    
-        if (!value) return
+        this.onDisabled()
 
         value.split(/, ?/g).forEach((entry, idx) => {
             const [ name, params ] = entry.split("-")
@@ -63,23 +66,28 @@ void new class extends Feature {
     
             this.whiteList.put(clazz, healthList)
         })
-
-        World.getWorld()?./* loadedEntityList */field_72996_f?.forEach(this.test.bind(this))
     }
 
     onDisabled() {
-        this.onEnabled(false)
+        this.outliner.clear()
+        this.whiteList.clear()
+    }
+
+    onUnregister() {
+        this.outliner.clear()
     }
 
     postInit() {
         this.color.update()
 
-        // this.tester = createCustomOutlineTester(this.test.bind(this)),
-        // this.outliner = createPerEntityOutliner(this.tester, this.color.packed, 2)
+        this.tester = createCustomOutlineTester(this.test.bind(this)),
+        this.outliner = createPerEntityOutliner(this.tester, this.color.packed, 2)
+
+        this.outliner.setColor(this.color.packed)
+        this.color._registerListener(() => this.outliner.setColor(this.color.packed))
+        
         this.outliner.register()
-        // this.outliner.setColor(this.color.packed)
-        // this.color._registerListener(() => this.color.packed)
-    
+
         ConfigProperty.getConfig().onCloseGui(this.onEnabled.bind(this))
     }
 }
