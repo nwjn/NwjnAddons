38c79f7fe69642d4550cb7d09b4c27c1958d11d7
Cameron Lee <127889040+nwjn@users.noreply.github.com>
2025-05-28 20:19:49 -0700

    ChatWaypoints: Fix conc mod exception

--- features/General/ChatWaypoints.js
+++ features/General/ChatWaypoints.js
@@ -2,7 +2,7 @@ import Data from "../../data/Data"
 import Nwjn from "../../libs/Helper/Nwjn"
 import Feature from "../../libs/Features/Feature"
 import TextUtil from "../../libs/Helper/TextUtil"
-import Waypoint from "../../libs/Render/TempWaypoint"
+import Waypoint from "../../libs/Helper/TempWaypoint"
 import ConfigProperty from "../../data/ConfigProperty"
 
 void new class extends Feature {
@@ -16,13 +16,13 @@ void new class extends Feature {
                 description: "Creates waypoints taken from chat messages in patcher coords format"
             }),
 
-            color: new ConfigProperty("Switch", {
+            color: new ConfigProperty("ColorPicker", {
                 category: "General",
                 subcategory: "Waypoints",
                 configName: "ChatWaypointsColor",
                 title: "➤ Waypoint Color",
                 description: "     Sets the color for waypoints",
-                value: [255, 190, 239, 200],
+                value: [255, 190, 239, 178],
                 shouldShow: data => data.ChatWaypoints
             }),
 
@@ -33,26 +33,25 @@ void new class extends Feature {
                 title: "➤ Waypoint Time",
                 description: "     The amount of seconds waypoints should stay",
                 options: [30, 90],
-                value: 120,
+                value: 60,
                 shouldShow: data => data.ChatWaypoints
             }),
 
             WAYPOINT_REGEX: /^(?:[\w\-]{5} > )?(?:\[\d{1,3}\] .? ?)?(?:\[\w+\+*\] )?(\w{1,16})(?: .? ?)?: x: (-?[\d\.]+), y: (-?[\d\.]+), z: (-?[\d\.]+) ?(.+)?$/,
 
-            waypoints: new Map()
+            waypoints: new HashMap()
         })
 
         this.addEvent("ServerChat", this.onWaypointSent.bind(this), { setCriteria: this.WAYPOINT_REGEX })
 
-        this.addSubEvent("Step", this.onIntervalPassed.bind(this), () => this.waypoints.size, { setFps: 3 })
-        this.addSubEvent("RenderWorld", this.onRenderWorld.bind(this), () => this.waypoints.size)
+        this.addSubEvent("RenderWorld", this.onRenderWorld.bind(this), () => !this.waypoints.isEmpty())
     }
 
     /**
      * @Event ServerChat
      * @Modifier WAYPOINT_REGEX
      */
-    onWaypointSent(displayName, x, y, z, text = "", {event, formatted}) {
+    onWaypointSent(displayName, x, y, z, text = "", { event, formatted }) {
         const ign = TextUtil.getSenderName(displayName).toLowerCase()
 
         if (ign in Data.blacklist) return Nwjn.append(event./* getChatComponent */func_148915_c(), "§cBlacklisted")
@@ -60,32 +59,24 @@ void new class extends Feature {
         const [mainText] = TextUtil.getMatches(/^(.+)§.:/, formatted)
         if (!mainText) return
 
-        this.waypoints.set(ign, new Waypoint(mainText, text, x, y, z, color, 5, this.time.value))
+        const waypoint = new Waypoint(ign, mainText, text, x, y, z, this.color, 5, this.time.value)
+            .onDelete(() => {
+                this.subEvents[0][0].unregister()
+                Client.scheduleTask(() => {
+                    this.waypoints.remove(ign)
+                    this.update()
+                })
+            })
+
+        this.waypoints.put(ign, waypoint)
         this.update()
-    }
-
-    /**
-     * @Event Step
-     * @Modifier 3 FPS
-     */
-    onIntervalPassed() {
-        this.waypoints.forEach((waypoint, id) => {
-            waypoint.update()
-
-            if (!waypoint.dirty) return
-            
-            this.waypoints.delete(id)
-            this.update()
-        })
     }
 
     /**
      * @Event RenderWorld
      */
-    onRenderWorld() {
-        this.waypoints.forEach(waypoint => 
-            waypoint.render()
-        )
+    onRenderWorld(pTicks) {
+        this.waypoints.forEach((_, waypoint) => waypoint.render(pTicks))
     }
 
     onDisabled() {
