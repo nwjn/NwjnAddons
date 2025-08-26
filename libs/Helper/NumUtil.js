5221f226d8ee666f6315b2bfb187df8fb5a7aa76
Cameron Lee <127889040+nwjn@users.noreply.github.com>
2025-08-26 14:00:45 -0700

    Integrate Color into NumUtil

--- libs/Helper/NumUtil.js
+++ libs/Helper/NumUtil.js
@@ -29,11 +29,29 @@ CompactDE.setMaximumFractionDigits(3)
 
 const suffixes = "kmbtq"
 
+const colors = [
+    ["§4", 0xAA0000FF],
+    ["§c", 0xFF5555FF],
+    ["§6", 0xFFAA00FF],
+    ["§e", 0xFFFF55FF],
+    ["§a", 0x55FF55FF],
+    ["§2", 0x00AA00FF]
+]
+
 export default class NumUtil {
     static FORMAT = { US: 0, DE: 1 }
 
     static isFormatUS() {
         return GroupingFormat.value === 0
+    }
+
+    static swapFormat(string) {
+        for (let idx in string) {
+            let char = string[idx]
+
+            if (char === ".") string[idx] = ","
+            else if (char === ",") string[idx] = "."
+        }
     }
 
     static RADIAN = Math.PI / 180
@@ -70,5 +88,42 @@ export default class NumUtil {
         const magnitude = suffixes.indexOf(match[2]) + 1
 
         return number * Math.pow(10, 3 * magnitude)
+    }
+
+    static scaleColorCode(num, min, max) {
+        const percent = ~~((num - min) / (max - min)) * 100
+        const low = ~~(percent / 17)
+
+        if (low >= colors.length) return colors[colors.length - 1][0]
+
+        return colors[low][0]
+    }
+
+    static scaleHexCode(num, min, max) {
+        const percent = ~~((num - min) / (max - min)) * 100
+        const low = ~~(percent / 17)
+        const high = low + 1
+
+        if (high >= colors.length) return colors[colors.length - 1][1]
+
+        const factor = percent / 17 - low
+        return this.lerpColor(colors[low][1], colors[high][1], factor)
+    }
+
+    static lerpColor(color1, color2, multi) {
+        const r1 = (color1 >> 24) & 0xff
+        const g1 = (color1 >> 16) & 0xff
+        const b1 = (color1 >> 8) & 0xff
+
+        const r2 = (color2 >> 24) & 0xff
+        const g2 = (color2 >> 16) & 0xff
+        const b2 = (color2 >> 8) & 0xff
+
+        const r = (r1 + (r2 - r1) * multi) << 24
+        const g = (g1 + (g2 - g1) * multi) << 16
+        const b = (b1 + (b2 - b1) * multi) << 8
+        const a = 0xff
+
+        return r + g + b + a
     }
 }
